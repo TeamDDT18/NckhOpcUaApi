20356bbb5842aad6de8260b19c5c3e2832d6b235
TeamDDT18 <100427555+TeamDDT18@users.noreply.github.com>
2022-04-21 06:57:17 +0700

    update client

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { AccountService } from './../services/account.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any={}
+  loggedIn: boolean;
+  constructor(private accountService: AccountService ) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.accountService.login(this.model).subscribe(response =>
+      {
+        console.log(response);
+        this.loggedIn = true;
+      }, error =>
+      {
+        console.log(error);
+      })
+  }
+  logout(){
+    this.loggedIn = false;
+  }
+  
+  
+}
+
